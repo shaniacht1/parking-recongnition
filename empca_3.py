6330dabe972641bb2a9baba7a73e5bd1b5eef40c
Shani Acht <shaniacht1@gmail.com>
2017-08-29 12:21:49 +0300

    Initial commit

--- empca_3.py
+++ empca_3.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 
 class empca():
-    def __init__(self,A, m, n, max_iter, max_err, k):
+    def __init__(self,A, max_iter, max_err, k):
         # A = A.T
         self.N = A.shape[0]
         self.d = A.shape[1]
@@ -29,7 +29,7 @@ class empca():
         j=0
         while j<k:
             print 'while iter num ', j
-            self.V[:,j] = randomVector
+            self.V[:,j] = randomVector.copy()
             i = 0
             while i < max_iter:
                 print 'entering second while'
@@ -37,10 +37,9 @@ class empca():
                 sum  = np.zeros((1, self.d), dtype=np.float)
                 for m in range(0, self.N):
                     c = np.dot(self.Mod_A[m,:], u0)
-                    sum += np.dot(c, self.Mod_A[m,:])
+                    sum += np.dot(self.Mod_A[m,:], c)
                 print 'after for'
                 self.V[:,j]= self.normalize(sum.ravel())
-                print 'after ravel'
                 if i>10 and np.max(np.abs(self.V[:,j] - u0)) <= max_err:
                     break
                 print 'after if'
@@ -51,11 +50,11 @@ class empca():
             j += 1
 
         alpha = np.dot(self.V.T, self.A.T)
-        print 'cha cha cha: ', np.cov(alpha)
+        # print 'cha cha cha: ', np.cov(alpha)
         temp = np.cov(alpha)
         self.S, eigVe = np.linalg.eigh(np.dot(0.02, temp))
         print 'fdfd'
-        print eigVe
+        # print eigVe.shape
         print self.V
         print self.S
         self.V = np.dot(self.V, eigVe).T
