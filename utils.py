e88e24fa24ec9cbb089a3362665163d223420ce8
Shani Acht <shaniacht1@gmail.com>
2017-10-07 17:19:43 +0300

    Initial commit

--- utils.py
+++ utils.py
@@ -1,18 +1,15 @@
-from PIL import Image
-#from numpy import *
+import math
+import glob
+import pickle
+import statistics
+import cv2
 from pylab import *
 from pylab import plt
-import pca
-import glob
 sys.path.append('/Users/shaniacht/anaconda/lib/python2.7/site-packages')
-import cv2
-from skimage.measure import ransac, LineModelND
 from skimage.morphology import skeletonize
 from skimage.measure import ransac, LineModelND
 from skimage import feature
-import statistics
-import pickle
-import math
+
 from Line import Line
 
 def getImagesFromDir(path):
@@ -28,56 +25,28 @@ def getDataMatrix(imlist):
     L = np.asarray(L).astype(np.float)
     return L.reshape(L.shape[0], -1)
 
-def straightenImage(im_src):
-    #im_src = cv2.imread(imlist[43])
-    pts_src = np.array([[301, 347], [479, 359], [249, 370], [439, 384]]).astype(np.float)
-    # pts_dst = np.array([[346, 323], [518, 323], [346, 377],[518, 377]]).astype(np.float)
-    pts_dst = np.array([[485, 475], [620, 475], [485, 510], [620, 510]]).astype(np.float)
+def straightenImage(
+        im_src,
+        src1 = [301, 347], src2 = [479, 359],
+        src3 = [249, 370], src4 = [439, 384],
+        dst1 = [485, 475], dst2 = [620, 475],
+        dst3 = [485, 510], dst4 = [620, 510]):
+
+    pts_src = np.array([src1, src2, src3, src4]).astype(np.float)
+    pts_dst = np.array([dst1, dst2, dst3, dst4]).astype(np.float)
 
     h, mask = cv2.findHomography(pts_src, pts_dst)
     im_dst = cv2.warpPerspective(im_src, h, (im_src.shape[1], im_src.shape[0]))
     return im_dst
 
-def recDetection(img, blank):
-    print 'fff',blank.shape
-    print 'ffff', img.shape
-    gray = cv2.cvtColor(blank, cv2.COLOR_BGR2GRAY)
-    gray = cv2.blur(gray, (3, 3))
-    #gray = cv2.GaussianBlur(gray, (1, 3), 0)
-    # recognize less, but less shtuyot (more accurTE)
-    ret3, edges = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # recognize more, but more shtuyot
-    #edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    boundRect = []
-    polygons = []
-    for i, cont in enumerate(contours):
-        # poly = cv2.approxPolyDP(cont, 3, True)
-        # polygons.append(poly)
-        x, y, w, h = cv2.boundingRect(cont)
-        if w < 200 and w > 100 and h > 20 and h < 60:
-            cv2.rectangle(img, (x,y),(x+w, y+h), (0, 255, 0), 2)
-    cv2.drawContours(img, contours, -1, (0, 0, 0), 1)
-    cv2.imshow("cha cha", img)
-    cv2.waitKey(0)
-
 def inliers_around_point(skeleton, x, y, win_w, win_h):
-    # print 'this is y: ',round(y)
-    # print 'this is win_h: ', win_h
     y = int(round(y))
-    # print len(skeleton)
     m, n = 740, 1024
-    # print 'this is m,n: ', m,n
     x1 = max(0, x-win_w)
     x2 = min(n-1, x+win_w)
     y1 = max(0, y-win_h)
     y2 = min(m-1, y+win_h)
-    # print x1 , x2, y1, y2
-    # window_area = np.abs((x1-x2)*(y1-y2))
     cur_window = skeleton[y1 : y2, x1 : x2]
-    # plt.imshow(cur_window)
-    # plt.show()
-    # print 'nonzero in window:', count_nonzero(cur_window)
     return count_nonzero(cur_window)
 
 def crop_lines(model_robust, skeleton, x, y, win_w, win_h):
@@ -99,9 +68,7 @@ def crop_lines(model_robust, skeleton, x, y, win_w, win_h):
         isLine = True
         start_x = 0
     # find line crops:
-    # for i in range(win_w, x.shape[0]):
     while i < x.shape[0]:
-        # print i
         inliers_count = inliers_around_point(skeleton, i, int(y[i]), win_w, win_h) > (win_w*2-5)
         if isLine:
             if inliers_count:
@@ -114,7 +81,6 @@ def crop_lines(model_robust, skeleton, x, y, win_w, win_h):
                         print 'found intersection', i, y[i]
                         # remove short lines
                         if i - start_x > 50:
-                            # cropped.append((model_robust, start_x, i, y[start_x], y[i]))
                             newLine = Line(start_x, i, y[start_x], y[i], model_robust)
                             print 'gaga',newLine.x2, newLine.y2
                             cropped.append(Line(start_x, i, y[start_x], y[i], model_robust))
@@ -145,9 +111,6 @@ def crop_lines(model_robust, skeleton, x, y, win_w, win_h):
         cropped.append(Line(start_x, x.shape-1, y[start_x], y[x.shape-1], model_robust))
     return cropped
 
-def findIntersections(hor_lines, ver_lines):
-    return hor_lines
-
 def lineDetection(img):
     print 'img size: ', img.shape
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -175,27 +138,18 @@ def lineDetection(img):
         x2 = model_robust.predict_x(y2)
         # this is a vertical line
         if np.abs(y1[0] - y1[-1]) > 730 and np.abs(x2[0] - x2[-1]) < 20:
-            ver_lines.append((model_robust, np.abs(x2[0] - x2[-1])/2))
+            ver_lines.append(Line(x2[0], x2[-1], y2[0], y2[-1], model_robust))
         # this is a horizontal line
         if np.abs(y1[0] - y1[-1]) < 50 and np.abs(x2[0] - x2[-1]) > 1000:
             # hor_lines += [(model_robust, x1, y1)]
             cur_lines = crop_lines(model_robust, skeleton, x1, y1, 10, 10)
-            figure('dd')
+            figure('Line Markings')
             plt.scatter([x.x1 for x in cur_lines], [x.y1 for x in cur_lines], c="g")
             plt.scatter([x.x2 for x in cur_lines], [x.y2 for x in cur_lines], c="r")
             hor_lines += cur_lines
         edge_pts_xy = edge_pts_xy[~inliers]
     plt.imshow(skeleton)
     return hor_lines, ver_lines
-
-def y_value(line):
-    return line.y1
-
-def x_start_value(line):
-    return line.x1
-
-def x_end_value(line):
-    return line.x2
 
 def areNeigbours(line1, line2):
     if not line1 or not line2:
@@ -226,21 +180,21 @@ def find_parkings(img):
             next_line = sorted_hor_lines[i+1]
             is_next_neig = areNeigbours(cur_line, next_line)
         is_prev_neig = areNeigbours(cur_line, prev_line)
-        #  is_prev_neig &&  is_next_neig => line in the middle of row
-        #  is_prev_neig && !is_next_neig => line last of row
-        # !is_prev_neig &&  is_next_neig => line first of row
-        # !is_prev_neig && !is_next_neig => not a parking!
         if is_prev_neig:
+            #  is_prev_neig &&  is_next_neig => line in the middle of row
             if is_next_neig:
                 cur_x1 = statistics.median([prev_points[0][0],cur_x_start,next_line.x1])
                 cur_x2 = statistics.median([prev_points[1][0],cur_x_end,next_line.x2])
+            # is_prev_neig && !is_next_neig => line last of row
             else:
                 cur_x1 = cur_x_start if np.abs(prev_points[0][0]-cur_x_start) < 5  else prev_points[0][0]
                 cur_x2 = cur_x_end if np.abs(prev_points[1][0]-cur_x_start) < 5  else prev_points[1][0]
         else:
+            # !is_prev_neig &&  is_next_neig => line first of row
             if is_next_neig:
                 cur_x1 = cur_x_start
                 cur_x2 = cur_x_end
+            # !is_prev_neig && !is_next_neig => not a parking!
             else:
                 cur_line = None
                 cur_x1 = -1000
@@ -255,7 +209,7 @@ def find_parkings(img):
         prev_points = cur_points
         i+=1
 
-    print len(sorted_hor_lines)
+    # print len(sorted_hor_lines)
     print parkings
 
     pickle_out = open("parking_array.pickle","wb")
